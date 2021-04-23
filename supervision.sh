e670d1c6ee6d952f756061a451377edb8d66e0da
Eliott-2001 <eliott.lisle@edu.itescia.fr>
2021-04-23 14:22:19 +0200

    doc

--- supervision.sh
+++ supervision.sh
@@ -2,13 +2,14 @@
 #supervison.sh
 #define arguments (time, folder_log, access_log, error_log, max_memory);
 user=$(whoami)
-
 time=$1
 folder_log=~/$2
 echo $folder_log
 access_log=$folder_log/$3.txt
 error_log=$folder_log/$4.txt
 max_memory=$5
+
+#create a directory for the archive and get the size of the log's files
 
 if ([ -d $folder_log ])
 then
@@ -17,19 +18,24 @@ then
         echo "Directory $folder_log exists."
         outSize=$(find $access_log -printf "%s")
         errSize=$(find $error_log -printf "%s")
-
-        testSize=$(($outSize+$errSize))
+#display the size of the files
+        
         echo "$access_log size is $outSize bytes."
         echo "$error_log size is $errSize bytes."
-        echo "Total : $testSize bytes;"
         
+
+#date formatting
+
         now=$(date +"%Y-%m-%d %H:%M:%S")
+
+#check memory size
+
         if [[ $outSize -gt $max_memory ]] || [[ $errSize -gt $max_memory ]]
-        then
+        then # killing processes
             for p in $(pgrep -u $user generation.sh); do kill $p; done
 
             if [[ $outSize -gt $max_memory ]] && [[ $errSize -gt $max_memory ]]
-            then
+            then # archive log's files
                 echo "Both files size are bigger than the max allocated !"
                 $(cd $folder_log && ls -l)
                 echo $(cd $folder_log && tar -czvf "./$now.tar.gz" "$3.txt" "$4.txt")
